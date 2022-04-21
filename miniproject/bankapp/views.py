a02b3f84382738658f8fa78fd6adae3358b77ff9
shajilcbabu <shajilcbabu11@gmail.com>
2022-04-21 09:24:15 +0530

    adminpage changed

--- miniproject/bankapp/views.py
+++ miniproject/bankapp/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index1(request):
-    return render(request,'admin.html')
-def index2(request):
      return render(request,'login.html')
+def index2(request):
+    return render(request,'admin.html')
 def index3(request):
      return render(request,'target.html')
 
