1ff2e604cf02559ab8d85ff7d2747c5df68de7fd
shajilcbabu <shajilcbabu11@gmail.com>
2022-04-29 15:37:09 +0530

    admin changed

--- miniproject/bankapp/urls.py
+++ miniproject/bankapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .import views
 
-urlpatterns =[
-    path('index1/',views.index1),
-    path('index2/',views.index2),
-    path('index3/',views.index3),
+urlpatterns = [
+    path('index1/', views.index1),
+    path('index2/', views.index2),
+    path('index3/', views.index3),
 ]
--- miniproject/bankapp/views.py
+++ miniproject/bankapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index1(request):
-     return render(request,'login.html')
-def index2(request):
-    return render(request,'admin.html')
-def index3(request):
-     return render(request,'target.html')
+     return render(request, 'login.html')
 
+
+def index2(request):
+    return render(request, 'admin.html')
+
+
+def index3(request):
+     return render(request, 'target.html')
